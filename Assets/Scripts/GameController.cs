d5390e34315fa2ea5e22c98f569e0dd8c8be90d3
chuck <14jhwang@stu.edu.cn>
2015-12-01 23:07:09 +0800

    Initial git of all files

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+	public GameObject hazard;
+
+	public Vector3 spawnValues;
+
+	public int hazardCount;
+
+	public float spawnWait;
+
+	public float startWait;
+
+	public float waveWait;
+
+	private int score;
+
+	private bool gameOver;
+
+	private bool restart;
+
+	void Start ()
+	{
+		gameOver = false;
+		restart = false;
+		score = 0;
+		StartCoroutine (SpawnWaves ());
+	}
+	
+	void Update ()
+	{
+		if (restart) {
+			if (Input.GetKeyDown (KeyCode.R)) {
+				Application.LoadLevel (Application.loadedLevel);
+			}
+		}
+	}
+
+	IEnumerator SpawnWaves ()
+	{
+		yield return new WaitForSeconds (startWait);
+
+		while (true)
+		{
+			for (int i = 0; i < hazardCount; i++)
+			{
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (hazard, spawnPosition, spawnRotation);
+
+				yield return new WaitForSeconds (spawnWait);
+			}
+
+			yield return new WaitForSeconds	(waveWait);
+
+			if (gameOver) {
+				restart = true;
+				break;
+			}
+		}
+	}
+
+	public void AddScore (int newScoreValue)
+	{
+		score += newScoreValue;
+	}
+
+	public void GameOver ()
+	{
+		gameOver = true;
+	}
+
+	void OnGUI ()
+	{
+		GUI.Label (new Rect(20, 20, 100, 20), "Score: " + score);
+
+		if (gameOver) {
+			GUI.Label (new Rect(20, 50, 100, 20), "Game Over!");
+		}
+
+		if (restart) {
+			GUI.Label (new Rect(20, 80, 200, 20), "Press 'R' For Restart");
+		}
+	}
+}
+
+
+
+
+
+
+
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+
+public class PlayerController: MonoBehaviour 
+{
+	public float speed;
+	public float verticalTilt;
+	public float horizontalTilt;
+	
+	public Boundary boundary;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+
+	public float fireRate;
+
+	private float nextFire;
+
+	private int fireWeapon;
+
+	void Start ()
+	{
+		// Initial FireWeapon 0;
+		fireWeapon = 0;
+	}
+
+	void Update ()
+	{
+		// Fire
+		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+
+			if (fireWeapon == 0) {
+				Instantiate (shot, shotSpawn.position, Quaternion.identity); // as GameObject;
+			} else {
+				Vector3 position1 = shotSpawn.position;
+				Vector3 position2 = shotSpawn.position;
+				position1.x = position1.x - 0.3f;
+				position2.x = position2.x + 0.3f;
+				Instantiate (shot, position1, Quaternion.identity);
+				Instantiate (shot, position2, Quaternion.identity);
+			}
+
+			GetComponent<AudioSource>().Play ();
+		}
+
+		// FireWeapon
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			fireWeapon = (fireWeapon == 0) ? 1 : 0;
+		}
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+		rigidbody.velocity = movement * speed;
+
+		rigidbody.position = new Vector3
+		(
+			Mathf.Clamp (rigidbody.position.x, boundary.xMin, boundary.xMax),
+			0.0f,
+			Mathf.Clamp (rigidbody.position.z, boundary.zMin, boundary.zMax)
+		);
+
+		rigidbody.rotation = Quaternion.Euler (rigidbody.velocity.z * verticalTilt, 0.0f, rigidbody.velocity.x * -horizontalTilt);
+	}
+
+	void OnGUI ()
+	{
+		if (fireWeapon == 0) {
+			GUI.Label (new Rect(Screen.width - 70, Screen.height - 50, 50, 30), "Single");
+		} else {
+			GUI.Label (new Rect(Screen.width - 70, Screen.height - 50, 50, 30), "Double");
+		}
+	}
+}
